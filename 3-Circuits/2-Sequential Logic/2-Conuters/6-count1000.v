1af5198af1fa62b3d906f0300aa95070af673a6b
Wang Jianze <e0427605@u.nus.edu>
2021-09-11 23:39:52 +0800

    Create 6-count1000.v

---
+++ 3-Circuits/2-Sequential Logic/2-Conuters/6-count1000.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,
+    output OneHertz,
+    output [2:0] c_enable
+); //
+
+    reg [3:0] q0, q1, q2;
+    bcdcount counter0 (clk, reset, c_enable[0], q0);
+    bcdcount counter1 (clk, reset, c_enable[1], q1);
+    bcdcount counter2 (clk, reset, c_enable[2], q2);
+    assign c_enable = { q1 == 4'b1001 && q0 == 4'b1001, q0 == 4'b1001, 1'b1 };
+    assign OneHertz = (q2 == 4'b1001) && (q1 == 4'b1001) && (q0 == 4'b1001);
+
+endmodule

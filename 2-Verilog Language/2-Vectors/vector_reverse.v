da432931d2b31c8fc9407cb511e841c48c1170f7
Wang Jianze <e0427605@u.nus.edu>
2021-09-11 20:53:10 +0800

    Update and rename vectorr.v to vector_reverse.v

--- 2-Verilog Language/2-Vectors/vector_reverse.v
+++ 2-Verilog Language/2-Vectors/vector_reverse.v
@@ -1,9 +1,10 @@
-// Vector reverse.
-// assign out[7:0] = in[0:7]; does not work because Verilog does not allow vector bit ordering to be flipped.
 module top_module( 
     input [7:0] in,
     output [7:0] out
 );
+
+// Vector reverse.
+// assign out[7:0] = in[0:7]; does not work because Verilog does not allow vector bit ordering to be flipped.
 
     assign out = { in[0], in[1], in[2], in[3], in[4], in[5], in[6], in[7] };
     

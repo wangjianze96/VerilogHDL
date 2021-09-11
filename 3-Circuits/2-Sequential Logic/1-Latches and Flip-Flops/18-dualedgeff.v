8f969d7fdeb9a5a59a0e725aae46414895cea17a
Wang Jianze <e0427605@u.nus.edu>
2021-09-11 18:12:00 +0800

    Update 18-dualedgeff.v

--- 3-Circuits/2-Sequential Logic/1-Latches and Flip-Flops/18-dualedgeff.v
+++ 3-Circuits/2-Sequential Logic/1-Latches and Flip-Flops/18-dualedgeff.v
@@ -16,19 +16,33 @@ module top_module (
 endmodule
 
 // from solotion
-module top_module (
-    input clk,
-    input d,
-    output q
-);
-
-    reg [1:0] m;
-    always @(posedge clk) begin
-        m[0] <= d;
-    end
-    always @(negedge clk) begin
-        m[1] <= d;
-    end
-    assign q = clk ? m[0] : m[1];
+module top_module(
+	input clk,
+	input d,
+	output q);
+	
+	reg p, n;
+	
+	// A positive-edge triggered flip-flop
+    always @(posedge clk)
+        p <= d ^ n;
+        
+    // A negative-edge triggered flip-flop
+    always @(negedge clk)
+        n <= d ^ p;
+    
+    // Why does this work? 
+    // After posedge clk, p changes to d^n. Thus q = (p^n) = (d^n^n) = d.
+    // After negedge clk, n changes to p^n. Thus q = (p^n) = (p^p^n) = d.
+    // At each (positive or negative) clock edge, p and n FFs alternately
+    // load a value that will cancel out the other and cause the new value of d to remain.
+    assign q = p ^ n;
+    
+    
+	// Can't synthesize this.
+	/*always @(posedge clk, negedge clk) begin
+		q <= d;
+	end*/
+    
     
 endmodule

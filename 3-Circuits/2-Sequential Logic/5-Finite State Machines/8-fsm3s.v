f828729ae72d79e247813cf803ec4dcf79cb60a3
Wang Jianze <e0427605@u.nus.edu>
2021-09-17 19:53:10 +0800

    Create 8-fsm3s.v

---
+++ 3-Circuits/2-Sequential Logic/5-Finite State Machines/8-fsm3s.v
@@ -0,0 +1,25 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+
+    // State transition logic
+    parameter A=0, B=1, C=2, D=3;
+    reg [3:0] state, next_state;
+    
+    assign next_state[A] = state[A]&(~in) | state[C]&(~in);
+    assign next_state[B] = state[A]&(in) | state[B]&(in) | state[D]&(in);
+    assign next_state[C] = state[B]&(~in) | state[D]&(~in);
+    assign next_state[D] = state[C]&(in);
+
+    // State flip-flops with synchronous reset
+    always @(posedge clk) begin
+        if (reset) state <= 4'b0001;
+        else state <= next_state;
+    end
+
+    // Output logic
+    assign out = state[D];
+
+endmodule
